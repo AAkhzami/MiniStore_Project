3a048e53a7ee390b97715f1830590ae45efa7756
abdullrahman <lodragon71@gmail.com>
2026-07-09 17:14:51 +0400

    Add the first two files for the database

---
+++ queries/Create_tables.sql
@@ -0,0 +1,91 @@
+create Database MiniStoreDB;
+
+create table Users
+(
+	UserID int Primary key identity(1,1),
+	UserName varchar(250),
+	Password varchar(300),
+	CreatedAt datetime default GETDATE(),
+	IsActive bit
+);
+
+create table Categories
+(
+	CategoryID int primary key identity(1,1),
+	Name varchar(150) not null
+);
+create table Products
+(
+	ProductID int Primary Key identity(1,1),
+	Name varchar(250),
+	CategoryID int not null,
+	Price decimal(10,3) not null,
+	StockQuantity int not null,
+	IsActive bit not null default 1,
+    CreatedByUserID int 
+	
+	Constraint fk_Product_Category
+	foreign key (CategoryID) references Categories(CategoryID),
+	constraint fk_Product_User
+	foreign key(CreatedByUserID) references Users(UserID)
+);
+create table InventoryLogs
+(
+	LogID int primary key identity(1,1),
+	ProductID int not null,
+	OldStock int,
+	NewStock int,
+	ChangeDate datetime default GETDATE()
+
+	constraint fk_Inventory_Product
+	foreign key(ProductID) references Products(ProductID)
+);
+create table Customers
+(
+	CustomerID int primary key Identity(1,1),
+	CustomerName varchar(100),
+	PhoneNumber varchar(50),
+	CreatedByUserID int
+
+	constraint fk_Customer_User
+	foreign key (CreatedByUserID) references Users(UserID)
+);
+create table Orders
+(
+	OrderID int primary key identity(1,1),
+	CustomerID int not null,
+	OrderDate datetime not null default GETDATE(),
+	TotalAmount decimal(10,3) not null,
+	CreatedByUserID int
+
+	constraint fk_Order_User
+	foreign key(CreatedByUserID) references Users(UserID),
+	Constraint fk_Order_Customer
+	foreign key (CustomerID) references Customers(CustomerID)
+);
+create table OrderDetails
+(
+	DetailID int primary key identity(1,1),
+	OrderID int not null,
+	ProductID int not null,
+	Quantity int,
+	PricePerUnit decimal(10,3)
+
+	Constraint fk_OrderDetails_Order
+	foreign key (OrderID) references Orders(OrderID),
+	Constraint fk_OrderDetails_Products
+	foreign key (ProductID) references Products(ProductID)
+);
+
+
+create view v_CustomerOrders as
+select 
+c.CustomerName,
+c.PhoneNumber,
+o.OrderID,
+o.OrderDate,
+o.TotalAmount,
+o.CreatedByUserID
+from Customers c
+inner join Orders o on
+c.CustomerID = o.CustomerID;
---
+++ queries/SQLQuery1.sql
@@ -0,0 +1,123 @@
+------ Triggers
+
+create trigger trg_DeleteProduct on Products
+instead of Delete
+as
+begin
+	update p
+	set p.IsActive = 0
+	from Products p inner join deleted d on
+	p.ProductID = d.ProductID
+end;
+
+
+create trigger trg_InsertOnCustomerOrdersView on v_CustomerOrders
+instead of insert
+as
+begin
+	declare @NewCustomer table
+	(
+		ID int,
+		Name varchar(250)
+	);
+	
+	insert into Customers
+	(CustomerName,PhoneNumber,CreatedByUserID)
+	Output inserted.CustomerID,inserted.CustomerName into @NewCustomer(ID,Name)
+	select CustomerName,PhoneNumber,CreatedByUserID
+	from inserted;
+
+	insert into Orders
+	(CustomerID,OrderDate,TotalAmount,CreatedByUserID)
+	select n.ID, d.OrderDate,d.TotalAmount,d.CreatedByUserID
+	from inserted d inner join @NewCustomer n on
+	n.Name = d.CustomerName
+end;
+
+create trigger trg_AfterInsertOrderDetails on OrderDetails
+after insert
+as 
+begin
+		update p
+		set p.StockQuantity = p.StockQuantity - i.Quantity
+		from Products p
+		inner join inserted i on
+		p.ProductID = i.ProductID
+		where (p.StockQuantity - i.Quantity) >= 0;
+end;
+
+create trigger trg_AfterUpdateProductQuantity on Products
+after update
+as
+begin
+	insert into InventoryLogs
+	(ProductID,OldStock,NewStock)
+	select 
+	i.ProductID,
+	d.StockQuantity,
+	i.StockQuantity
+	from inserted i inner join deleted d on
+	i.ProductID = d.ProductID;
+end;
+
+------ Scalar Functions
+create Function dbo.FinalPrice(@Price decimal(10,3))
+Returns decimal(10,3)
+as
+begin
+	declare @FinalPrice decimal(10,3);
+
+	set @FinalPrice = @Price * 1.15;
+	RETURN @FinalPrice;
+end;
+
+create Function dbo.GetReportOfOrder
+(@OrderID int)
+Returns Table
+as
+return
+(
+select
+	p.Name,
+	od.Quantity,
+	od.PricePerUnit AS BasePrice,
+	dbo.FinalPrice(od.PricePerUnit) AS PriceAfterTax
+	from OrderDetails od
+	inner join Products p on
+	p.ProductID = od.ProductID
+	where od.OrderID = @OrderID
+);
+
+create Function dbo.GetReportOfProducts()
+Returns Table
+as
+return
+(
+	select
+	p.ProductID,
+	p.Name as ProductName,
+	p.Price ,
+	p.StockQuantity,
+	c.Name as CategoryName,
+	DENSE_RANK() over (PARTITION by c.Name Order by ISNULL(SUM(od.Quantity),0) DESC) as CategoryRank
+	from  Products p
+	inner join Categories c on
+	p.CategoryID = c.CategoryID
+	LEFT join OrderDetails od on
+	p.ProductID = od.ProductID
+	Group by p.ProductID, p.Name,p.Price,p.StockQuantity,c.Name
+);
+
+create Function dbo.ProductsPagination
+(@pageNumber int, @RowPerPage int)
+Returns Table
+as
+return
+(
+	select 
+	ProductID, Name, Price, StockQuantity, IsActive
+	from Products
+	Order by ProductID
+	offset (@pageNumber - 1) * @RowPerPage ROWS
+	Fetch next @RowPerPage ROWS Only
+);
