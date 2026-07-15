3840bbbb34798ad07d9bf80e0e9e5995ce84761a
abdullrahman <lodragon71@gmail.com>
2026-07-15 21:32:48 +0400

    Reintroduce and enhance ctrlCustomerInfo control
    Reintroduced the `ctrlCustomerInfo` user control with a new design and functionality:
    - Added `Reset` and `LoadData` methods for managing customer data.
    - Integrated `clsCustomers` and `MiniStoreDB_Business_Layer` dependencies.
    - Updated the layout with a `Guna2GroupBox` and visual elements for customer fields.
    
    Updated project structure and resources:
    - Moved `ctrlCustomerInfo` files to `Customers\Control`.
    - Added `phone-call.png` resource and removed `down.png`.
    - Updated `MiniStore.csproj` to reflect file changes.
    
    Modified layout files (`DocumentLayout.backup.json` and `DocumentLayout.json`) to adjust document metadata and indices.
    
    Recompiled project, resulting in updates to binary files.

---
+++ MiniStore/Customers/Control/ctrlCustomerInfo.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace MiniStore.Customers
+{
+    partial class ctrlCustomerInfo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbProductInfo = new Guna.UI2.WinForms.Guna2GroupBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblCreatedBy = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblIsActive = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCustomerID = new System.Windows.Forms.Label();
+            this.lable1 = new System.Windows.Forms.Label();
+            this.gbProductInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbProductInfo
+            // 
+            this.gbProductInfo.BorderRadius = 9;
+            this.gbProductInfo.Controls.Add(this.pictureBox6);
+            this.gbProductInfo.Controls.Add(this.pictureBox5);
+            this.gbProductInfo.Controls.Add(this.pictureBox3);
+            this.gbProductInfo.Controls.Add(this.pictureBox2);
+            this.gbProductInfo.Controls.Add(this.pictureBox1);
+            this.gbProductInfo.Controls.Add(this.lblCreatedBy);
+            this.gbProductInfo.Controls.Add(this.label10);
+            this.gbProductInfo.Controls.Add(this.lblIsActive);
+            this.gbProductInfo.Controls.Add(this.label8);
+            this.gbProductInfo.Controls.Add(this.lblPhoneNumber);
+            this.gbProductInfo.Controls.Add(this.label4);
+            this.gbProductInfo.Controls.Add(this.lblCustomerName);
+            this.gbProductInfo.Controls.Add(this.label2);
+            this.gbProductInfo.Controls.Add(this.lblCustomerID);
+            this.gbProductInfo.Controls.Add(this.lable1);
+            this.gbProductInfo.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(121)))), ((int)(((byte)(243)))));
+            this.gbProductInfo.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold);
+            this.gbProductInfo.ForeColor = System.Drawing.Color.White;
+            this.gbProductInfo.Location = new System.Drawing.Point(3, 3);
+            this.gbProductInfo.Name = "gbProductInfo";
+            this.gbProductInfo.Size = new System.Drawing.Size(465, 370);
+            this.gbProductInfo.TabIndex = 15;
+            this.gbProductInfo.Text = "Customer Information";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::MiniStore.Properties.Resources.user_31;
+            this.pictureBox6.Location = new System.Drawing.Point(220, 301);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(28, 28);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 34;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::MiniStore.Properties.Resources.check;
+            this.pictureBox5.Location = new System.Drawing.Point(220, 242);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(28, 28);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 33;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::MiniStore.Properties.Resources.phone_call;
+            this.pictureBox3.Location = new System.Drawing.Point(220, 183);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(28, 28);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 31;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::MiniStore.Properties.Resources.user_3;
+            this.pictureBox2.Location = new System.Drawing.Point(220, 124);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(28, 28);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 30;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MiniStore.Properties.Resources.price_tag;
+            this.pictureBox1.Location = new System.Drawing.Point(220, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(28, 28);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 29;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblCreatedBy
+            // 
+            this.lblCreatedBy.AutoSize = true;
+            this.lblCreatedBy.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.lblCreatedBy.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(115)))), ((int)(((byte)(138)))));
+            this.lblCreatedBy.Location = new System.Drawing.Point(270, 304);
+            this.lblCreatedBy.Margin = new System.Windows.Forms.Padding(30, 30, 30, 10);
+            this.lblCreatedBy.Name = "lblCreatedBy";
+            this.lblCreatedBy.Size = new System.Drawing.Size(52, 22);
+            this.lblCreatedBy.TabIndex = 28;
+            this.lblCreatedBy.Text = "NULL";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(29)))), ((int)(((byte)(63)))));
+            this.label10.Location = new System.Drawing.Point(27, 301);
+            this.label10.Margin = new System.Windows.Forms.Padding(15);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(130, 29);
+            this.label10.TabIndex = 27;
+            this.label10.Text = "Created By";
+            // 
+            // lblIsActive
+            // 
+            this.lblIsActive.AutoSize = true;
+            this.lblIsActive.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.lblIsActive.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(115)))), ((int)(((byte)(138)))));
+            this.lblIsActive.Location = new System.Drawing.Point(270, 245);
+            this.lblIsActive.Margin = new System.Windows.Forms.Padding(30, 30, 30, 10);
+            this.lblIsActive.Name = "lblIsActive";
+            this.lblIsActive.Size = new System.Drawing.Size(52, 22);
+            this.lblIsActive.TabIndex = 26;
+            this.lblIsActive.Text = "NULL";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(29)))), ((int)(((byte)(63)))));
+            this.label8.Location = new System.Drawing.Point(27, 242);
+            this.label8.Margin = new System.Windows.Forms.Padding(15);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(104, 29);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "Is Active";
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.AutoSize = true;
+            this.lblPhoneNumber.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.lblPhoneNumber.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(115)))), ((int)(((byte)(138)))));
+            this.lblPhoneNumber.Location = new System.Drawing.Point(270, 186);
+            this.lblPhoneNumber.Margin = new System.Windows.Forms.Padding(30, 30, 30, 10);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(52, 22);
+            this.lblPhoneNumber.TabIndex = 22;
+            this.lblPhoneNumber.Text = "NULL";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(29)))), ((int)(((byte)(63)))));
+            this.label4.Location = new System.Drawing.Point(27, 183);
+            this.label4.Margin = new System.Windows.Forms.Padding(15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 29);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Phone Number ";
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.lblCustomerName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(115)))), ((int)(((byte)(138)))));
+            this.lblCustomerName.Location = new System.Drawing.Point(267, 127);
+            this.lblCustomerName.Margin = new System.Windows.Forms.Padding(30, 30, 30, 10);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(52, 22);
+            this.lblCustomerName.TabIndex = 20;
+            this.lblCustomerName.Text = "NULL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(29)))), ((int)(((byte)(63)))));
+            this.label2.Location = new System.Drawing.Point(27, 124);
+            this.label2.Margin = new System.Windows.Forms.Padding(15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 29);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Customer Name";
+            // 
+            // lblCustomerID
+            // 
+            this.lblCustomerID.AutoSize = true;
+            this.lblCustomerID.Font = new System.Drawing.Font("Tahoma", 13F);
+            this.lblCustomerID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(115)))), ((int)(((byte)(138)))));
+            this.lblCustomerID.Location = new System.Drawing.Point(267, 73);
+            this.lblCustomerID.Margin = new System.Windows.Forms.Padding(30, 30, 30, 10);
+            this.lblCustomerID.Name = "lblCustomerID";
+            this.lblCustomerID.Size = new System.Drawing.Size(52, 22);
+            this.lblCustomerID.TabIndex = 18;
+            this.lblCustomerID.Text = "NULL";
+            // 
+            // lable1
+            // 
+            this.lable1.AutoSize = true;
+            this.lable1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lable1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(29)))), ((int)(((byte)(63)))));
+            this.lable1.Location = new System.Drawing.Point(27, 70);
+            this.lable1.Margin = new System.Windows.Forms.Padding(30, 30, 30, 10);
+            this.lable1.Name = "lable1";
+            this.lable1.Size = new System.Drawing.Size(147, 29);
+            this.lable1.TabIndex = 17;
+            this.lable1.Text = "Customer ID";
+            // 
+            // ctrlCustomerInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.gbProductInfo);
+            this.Name = "ctrlCustomerInfo";
+            this.Size = new System.Drawing.Size(471, 376);
+            this.gbProductInfo.ResumeLayout(false);
+            this.gbProductInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2GroupBox gbProductInfo;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblCreatedBy;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblIsActive;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblPhoneNumber;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCustomerID;
+        private System.Windows.Forms.Label lable1;
+    }
+}
---
+++ MiniStore/Customers/Control/ctrlCustomerInfo.cs
@@ -0,0 +1,48 @@
+﻿using MiniStoreDB_Business_Layer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniStore.Customers
+{
+    public partial class ctrlCustomerInfo : UserControl
+    {
+        clsCustomers _Customer;
+        public ctrlCustomerInfo()
+        {
+            InitializeComponent();
+        }
+        public void Reset()
+        {
+            _Customer = null;
+            lblCustomerID.Text = "NULL";
+            lblCustomerName.Text = "NULL";
+            lblPhoneNumber.Text = "NULL";
+            lblIsActive.Text = "NULL";
+            lblCreatedBy.Text = "NULL";
+        }
+        public void LoadData(int customerID)
+        {
+            _Customer = clsCustomers.Find(customerID);
+            if( _Customer != null )
+            {
+                lblCustomerID.Text = _Customer.CustomerID.ToString();
+                lblCustomerName.Text = _Customer.CustomerName;
+                lblPhoneNumber.Text = _Customer.PhoneNumber;
+                lblIsActive.Text = _Customer.IsActive.ToString();
+                lblCreatedBy.Text = clsUsers.Find(_Customer.CreatedByUserID).UserName;
+            }
+            else
+            {
+                MessageBox.Show("Customer Not Found!", "Not Found",MessageBoxButtons.OK,MessageBoxIcon.Error );
+                return;
+            }
+        }
+    }
+}
