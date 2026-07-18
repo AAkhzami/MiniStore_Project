218e67a865fa0249183fb4e3046a3028aba1720c
abdullrahman <lodragon71@gmail.com>
2026-07-18 20:52:37 +0400

    Add clsInvoice class for generating Word invoices
    A new `clsInvoice` class was added to the `MiniStore.Global`
    namespace. This class provides functionality to generate and
    save invoices as Word documents using the Microsoft Office
    Interop Word library.
    
    The `clsInvoice` class includes:
    - A constructor to initialize the file path.
    - A `DownloadInvoiceAsWordFile` method to create and save
      Word documents with the provided invoice text.
    
    The project file `MiniStore.csproj` was updated to include
    the new `clsInvoice.cs` file.

---
+++ MiniStore/Global/clsInvoice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace MiniStore.Global
+{
+    internal class clsInvoice
+    {
+        string _pathFile;
+        public clsInvoice(string pathFile)
+        {
+            _pathFile = pathFile;
+        }
+
+        public bool DownloadInvoiceAsWordFile(string invoiceText)
+        {
+            Word.Application wordApp = new Word.Application();
+            try
+            {
+                wordApp.Visible = false;
+
+
+                Word.Document doc = wordApp.Documents.Add();
+                Word.Paragraph paragraph = doc.Paragraphs.Add();
+                paragraph.Range.Text = invoiceText;
+
+                string pathfile = _pathFile + "\\";
+                doc.SaveAs2(pathfile);
+                doc.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
