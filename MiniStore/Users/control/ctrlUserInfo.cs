9556aaf119d5b0c047d205b7b3dcd0a36c155457
abdullrahman <lodragon71@gmail.com>
2026-07-18 13:55:57 +0400

    Add ctrlUserInfo user control to MiniStore project
    Added a new user control `ctrlUserInfo` to display user details,
    including its implementation, designer file, and resources.
    Updated `MiniStore.csproj` to include the new control and its
    dependencies. Modified `DocumentLayout.json` and related files
    to reflect the addition of `ctrlUserInfo`.
    
    The `ctrlUserInfo` control provides methods to load and reset
    user data, displaying fields such as `UserID`, `UserName`,
    `FullName`, `CreatedAt`, and `IsActive`. Updated binary files
    and project caches to reflect the changes. Removed references
    to `frmAddUpdateUser` in layout files and adjusted document
    structure accordingly.

---
+++ MiniStore/Users/control/ctrlUserInfo.cs
@@ -0,0 +1,47 @@
+﻿using MiniStoreDB_Business_Layer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniStore.Users.control
+{
+    public partial class ctrlUserInfo : UserControl
+    {
+        clsUsers _user;
+        public ctrlUserInfo()
+        {
+            InitializeComponent();
+        }
+        public void LoadData(clsUsers user)
+        {
+            _user = user;
+            if(_user != null)
+            {
+                lblUserID.Text = _user.UserID.ToString();
+                lblUserName.Text = _user.UserName;
+                lblFullName.Text = _user.FullName;
+                lblCreatedAt.Text = _user.CreatedAt.ToString("dd/MM/YYYY");
+                lblIsActive.Text = _user.IsActive.ToString();
+            }
+            else
+            {
+                MessageBox.Show("User Not Found!","Not Found",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+        }
+        public void Reset()
+        {
+            lblUserID.Text = "NULL";
+            lblUserName.Text = "NULL";
+            lblFullName.Text = "NULL";
+            lblCreatedAt.Text = "NULL";
+            lblIsActive.Text = "NULL";
+        }
+    }
+}
